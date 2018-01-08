ed45dfd915466d1747132c73ebc0cbb7bf03c166
Alex Bykov <me@alexbykov.ru>
2018-01-08 19:49:13 +0300

    added javadoc

--- library/src/main/java/ru/alexbykov/norecyclerviewadapter/NoRecyclerViewAdapter.java
+++ library/src/main/java/ru/alexbykov/norecyclerviewadapter/NoRecyclerViewAdapter.java
@@ -14,8 +14,10 @@ import java.util.List;
 /**
  * @author Alex Bykov
  *         Date: 08.01.2018.
- *         <p>
  *         You can contact me at me@alexbykov.ru
+ *
+ * @version 0.0.1
+ *
  */
 
 public abstract class NoRecyclerViewAdapter<M, VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
@@ -29,25 +31,51 @@ public abstract class NoRecyclerViewAdapter<M, VH extends RecyclerView.ViewHolde
         return items.size();
     }
 
+
+    /**
+     * This method return item by position
+     *
+     * @param position is position which you want to get
+     * @return item
+     */
     public M getItem(int position) {
         return items.get(position);
     }
 
+
+    /**
+     * This method is add item by position
+     *
+     * @param item     is item what you want to add
+     * @param position is position of the item
+     */
     public void add(M item, int position) {
         items.add(position, item);
         notifyItemInserted(position);
     }
 
+
+    /**
+     * This methid is add item to the end of ArrayList
+     *
+     * @param item is item what you want to add
+     */
     public void add(M item) {
         items.add(item);
         notifyItemInserted(getLastPosition());
     }
+
 
     private int getLastPosition() {
         return items.size() - 1;
     }
 
 
+    /**
+     * This method remove element by position
+     *
+     * @param position is position which you want remove
+     */
     public void remove(int position) {
         try {
             notifyItemRemoved(position);
@@ -58,16 +86,32 @@ public abstract class NoRecyclerViewAdapter<M, VH extends RecyclerView.ViewHolde
 
     }
 
+    /**
+     * This method clear all items and update recyclerView state
+     */
     public void clear() {
         items.clear();
         notifyDataSetChanged();
     }
 
+    /**
+     * This method add items to the end of adapter
+     *
+     * @param items
+     */
     public void addItems(@NonNull List<M> items) {
         this.items.addAll(items);
         notifyDataSetChanged();
     }
 
+
+    /**
+     * This method inflate and return view
+     *
+     * @param viewGroup is viewGroup from method onCreateViewHolder
+     * @param layoutID  is LAYOUT which you want to inflate
+     * @return new View
+     */
     protected View inflate(@NonNull ViewGroup viewGroup, @LayoutRes int layoutID) {
         return LayoutInflater.from(viewGroup.getContext()).inflate(layoutID, viewGroup, false);
     }
